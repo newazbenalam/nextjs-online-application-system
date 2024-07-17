5366f9e391c28f261fc40f78dcc5004df37ed9b8
newazbenalam <newazbenalam@gmail.com>
2024-07-18 02:48:13 +0600

    add: form with localstorage

--- app/application/vnoc/form/page.jsx
+++ app/application/vnoc/form/page.jsx
@@ -3,136 +3,177 @@
 import Link from "next/link";
 import Script from "next/script";
 import React, { useEffect, useState } from "react";
-import { GetNOCtypes } from "@/app/lib/actions/GetNocTypes";
+import { useSearchParams } from "next/navigation";
 
 export default function ApplicationPage() {
-  const [nocTypes, setNocTypes] = useState([]);
+  const [nocType, setNocType] = useState(useSearchParams().get("nocTypeId"));
 
   useEffect(() => {
-    const getNocTypes = async () => {
-      const res = await GetNOCtypes();
-      setNocTypes(res);
-    };
-
-    getNocTypes();
+    //restore data from local storage
+    const personalInfo = JSON.parse(localStorage.getItem("personalInfo"));
+    if (personalInfo) {
+      document.getElementById("applicantName").value = personalInfo.applicantName;
+      document.getElementById("email").value = personalInfo.email;
+      document.getElementById("applicantPhone").value = personalInfo.applicantPhone;
+      document.getElementById("address").value = personalInfo.address;
+      document.getElementById("orgAddress").value = personalInfo.orgAddress;
+      document.getElementById("orgEmail").value = personalInfo.orgEmail;
+      document.getElementById("orgMobile").value = personalInfo.orgMobile;
+      document.getElementById("orgName").value = personalInfo.orgName;
+      document.getElementById("expirationDate").value = personalInfo.expirationDate;
+    }
   }, []);
 
-  const foo = () => {
-    // redirect to login page
-    window.location.href = "/application/payment";
+  const onSubmitFormToNextPage = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formDataObj = Object.fromEntries(formData.entries());
+    console.log("Form data:", formDataObj);
+
+    // save data to local storage
+    localStorage.setItem("personalInfo", JSON.stringify(formDataObj));
+
+    // window.location.href = "/application/payment";
   };
 
   return (
     <>
-      <main className="main-content container " style={{ marginTop: "100px" }}>
-        <section>
-          <form>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label
-                    for="example-password-input"
-                    class="form-control-label"
-                  >
-                    Password
-                  </label>
-
+      <main className="container" style={{ marginTop: "75px" }}>
+        <section className="rounded-3 card mb-2">
+          <p className="navbar-brand font-weight-bold m-auto py-2">
+            {"Personal Information"}
+          </p>
+        </section>
+        <section className="card">
+          <form className="m-4" onSubmit={onSubmitFormToNextPage}>
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Full Name</label>
                   <input
-                    type="email"
-                    class="form-control"
-                    id="exampleFormControlInput1"
-                    placeholder="name@example.com"
+                    type="text"
+                    className="form-control"
+                    id="applicantName"
+                    name="applicantName"
+                    placeholder="Full Name As Per NID"
                   />
                 </div>
               </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label
-                    for="example-password-input"
-                    class="form-control-label"
-                  >
-                    Password
-                  </label>
-
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    placeholder="example@gmail.com"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Phone</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="applicantPhone"
+                    name="applicantPhone"
+                    placeholder="01500000125"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Address</label>
                   <input
                     type="text"
-                    placeholder="Regular"
-                    class="form-control"
+                    className="form-control"
+                    id="address"
+                    name="address"
+                    placeholder="Dhaka, Bangladesh"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Organization Address</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="orgAddress"
+                    name="orgAddress"
+                    placeholder="Dhaka, Bangladesh"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Organization Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="orgEmail"
+                    name="orgEmail"
+                    placeholder="example@org.com"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Organization Mobile</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="orgMobile"
+                    name="orgMobile"
+                    placeholder="01500000125"
+                  />
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>NOC Types Title</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="nocTypesTitle"
+                    name="nocTypesTitle"
+                    placeholder="NOC Type Title"
+                    value={nocType}
                     disabled
                   />
                 </div>
               </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label
-                    for="example-password-input"
-                    class="form-control-label"
-                  >
-                    Password
-                  </label>
-
-                  <div class="input-group mb-4">
-                    <span class="input-group-text">
-                      <i class="ni ni-zoom-split-in"></i>
-                    </span>
-                    <input
-                      class="form-control"
-                      placeholder="Search"
-                      type="text"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <label
-                    for="example-password-input"
-                    class="form-control-label"
-                  >
-                    Password
-                  </label>
-
-                  <div class="input-group mb-4">
-                    <input
-                      class="form-control"
-                      placeholder="Birthday"
-                      type="text"
-                    />
-                    <span class="input-group-text">
-                      <i class="ni ni-zoom-split-in"></i>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-md-6">
-                <div class="form-group has-success">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Organization Name</label>
                   <input
                     type="text"
-                    placeholder="Success"
-                    class="form-control is-valid"
+                    className="form-control"
+                    id="orgName"
+                    name="orgName"
+                    placeholder="Organization Name"
                   />
                 </div>
               </div>
-              <div class="col-md-6">
-                <div class="form-group has-danger">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label>Expiration Date</label>
                   <input
-                    type="email"
-                    placeholder="Error Input"
-                    class="form-control is-invalid"
+                    type="date"
+                    className="form-control"
+                    id="expirationDate"
+                    name="expirationDate"
                   />
                 </div>
               </div>
             </div>
-            <Link href={"/"} className="me-2">
+            <Link href="/" className="me-2">
               <button type="reset" className="btn btn-default">
                 Back
               </button>
             </Link>
-
             <button type="submit" className="btn bg-gradient-primary">
               Next
             </button>
